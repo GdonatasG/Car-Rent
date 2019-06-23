314323fa089d0a93594101b7fd68b9273501421f
GDonatasG <zitkusd@gmail.com>
2019-06-23 12:14:05 +0300

    Register Activity layout: Logo, EditText`s.

--- app/src/main/java/com/android/carrent/activities/RegisterActivity.kt
+++ app/src/main/java/com/android/carrent/activities/RegisterActivity.kt
@@ -4,10 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.android.carrent.R
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
     }
 }
