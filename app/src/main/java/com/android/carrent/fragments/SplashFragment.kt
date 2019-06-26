1e8779f783c264f8f237edbd6e3da30daad414d5
GDonatasG <zitkusd@gmail.com>
2019-06-26 15:35:33 +0300

    SplashFragment layout.

--- app/src/main/java/com/android/carrent/fragments/SplashFragment.kt
+++ app/src/main/java/com/android/carrent/fragments/SplashFragment.kt
@@ -8,29 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.android.carrent.R
+import com.android.carrent.utils.changeFragment
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_splash.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class SplashFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var mAuth: FirebaseAuth? = null
+    private lateinit var loginFragment: LoginFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        mAuth = FirebaseAuth.getInstance()
+        loginFragment = LoginFragment()
     }
 
     override fun onCreateView(
@@ -38,27 +27,13 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        var v: View = inflater.inflate(R.layout.fragment_splash, container, false)
+
+        v.iv_logo.setOnClickListener {
+            mAuth?.signOut()
+            changeFragment(loginFragment)
+        }
+        return v
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SplashFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SplashFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
