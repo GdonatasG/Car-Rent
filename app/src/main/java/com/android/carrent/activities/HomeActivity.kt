6dc32a8cc144bb089cd1b96d0580b251b5968307
GDonatasG <zitkusd@gmail.com>
2019-07-01 20:54:14 +0300

    Changed navigation drawer into bottom navigation.

--- app/src/main/java/com/android/carrent/activities/HomeActivity.kt
+++ app/src/main/java/com/android/carrent/activities/HomeActivity.kt
@@ -1,141 +1,68 @@
 package com.android.carrent.activities
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.android.carrent.R
 import com.android.carrent.fragments.home.HomeFragment
-import com.android.carrent.models.User
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.android.carrent.fragments.home.ProfileFragment
+import com.android.carrent.fragments.home.RentedFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.app_bar_home.*
-import kotlinx.android.synthetic.main.nav_header_home.view.*
 
-class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class HomeActivity : AppCompatActivity() {
     private var TAG: String = "HomeActivity"
-
-    // Firebase
-    private var mAuth: FirebaseAuth? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        setSupportActionBar(toolbar)
 
-        // Init firebase and user
-        mAuth = FirebaseAuth.getInstance()
-        if (mAuth == null) {
-            Log.d(TAG, "User is not logged in, starting MainActivity(LoginFragment)")
-            startMainActivity()
-        } else initUser()
+        if (savedInstanceState == null) {
+            setFragment(HomeFragment())
+        }
 
-        // Setting default fragment
-        setFragmentView(fragment = HomeFragment())
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                Log.d(TAG, "Clicked on BottomNavigation home item")
+                setFragment(fragment = HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                Log.d(TAG, "Clicked on BottomNavigation profile item")
+                setFragment(fragment = ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_rented -> {
+                Log.d(TAG, "Clicked on BottomNavigation rented item")
+                setFragment(fragment = RentedFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_logout -> {
+                Log.d(TAG, "Clicked on BottomNavigation logout item")
+                // Return false, because no reason to select this item, it`s only logout button
+                return@OnNavigationItemSelectedListener false
+            }
 
-        nav_view.setNavigationItemSelectedListener(this)
+        }
+        false
     }
 
     override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
+        if (bottom_navigation.selectedItemId == R.id.navigation_home) {
             super.onBackPressed()
-        }
+        } else bottom_navigation.selectedItemId = R.id.navigation_home
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.home, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_home -> {
-                setFragmentView(fragment = HomeFragment())
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    private fun setFragmentView(fragment: Fragment) {
+    private fun setFragment(fragment: Fragment) {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.container, fragment)
             .commit()
-    }
-
-    private fun initUser() {
-        FirebaseDatabase.getInstance().getReference("users/${mAuth?.currentUser!!.uid}")
-            .addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(de: DatabaseError) {
-
-                }
-
-                override fun onDataChange(ds: DataSnapshot) {
-                    if (ds.exists()) {
-                        val user = ds.getValue(User::class.java)
-                        nav_view.tv_welcome.text = "" + resources.getText(R.string.nav_header_welcome) + " " +
-                                user?.username
-
-                        nav_view.tv_balance.text = user?.balance.toString() + " " +
-                                resources.getString(R.string.nav_header_currency_euro)
-                    }
-                }
-
-            })
-
-    }
-
-    private fun startMainActivity() {
-        finish()
-        startActivity(Intent(this, MainActivity::class.java))
     }
 }
